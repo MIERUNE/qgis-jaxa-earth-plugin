2a38e46aa2d63c62bc4150f4dce27ac7e2d5768e
Druk-Drink <94426824+Druk-Drink@users.noreply.github.com>
2023-09-26 15:47:57 +0900

    deleted comments

--- catalog/loader.py
+++ catalog/loader.py
@@ -11,13 +11,12 @@ def get_catalog() -> dict:
     run when build this plugin and make dictionary with output of this script
     """
     catalog = {}
-    
-    #
+
     res = requests.get(STAC_CATALOG_URL)
     res_json = res.json()
-    # print(res_json)  #add
-    
-    children = list(filter(lambda d: d["rel"] == "child", res_json.get("links", [])))
+
+    children = list(
+        filter(lambda d: d["rel"] == "child", res_json.get("links", [])))
     for child in children:
         res_child = requests.get(child["href"])
         res_child_json = res_child.json()
@@ -26,7 +25,7 @@ def get_catalog() -> dict:
         dataset_title = res_child_json["title"]
         dataset_bands = list(res_child_json["assets"].keys())
         dataset_keywords = res_child_json["keywords"]
-        dataset_bbox  = res_child_json["extent"]["spatial"]["bbox"] # +["bbox"][0]必要？
+        dataset_bbox = res_child_json["extent"]["spatial"]["bbox"]
 
         catalog[dataset_id] = {
             "title": dataset_title,
