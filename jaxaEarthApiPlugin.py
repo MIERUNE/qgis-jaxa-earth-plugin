e8f4e90c2bef54d569fd1574dbbfc5e23d7c9990
bordoray <bordoray@gmail.com>
2025-05-27 13:32:45 +0900

    Naming convention

--- jaxaEarthApiPlugin.py
+++ jaxaEarthApiPlugin.py
@@ -15,11 +15,9 @@
 import os
 
 # QGIS-API
-from qgis.core import *
-from qgis.gui import *
 from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
-from qgis.PyQt.QtGui import *
-from qgis.PyQt.QtWidgets import *
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction
 
 from .jaxaEarthApiDialog import JaxaEarthApiDialog
 
